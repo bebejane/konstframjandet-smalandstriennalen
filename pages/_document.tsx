eeebc940e0ff845e625b6b9736f403d45ee9ce18
Bebe Jane <bjornthief@gmail.com>
2023-04-24 15:34:54 +0200

    clean

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,6 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
-    const { year }: { year: YearExtendedRecord } = this.props?.__NEXT_DATA__?.props?.pageProps || {};
 
     return (
       <Html>
@@ -12,7 +11,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
         </Head>
-        <body style={{ backgroundColor: year?.isArchive ? 'var(--archive)' : 'var(--white)' }}>
+        <body>
           <Main />
           <NextScript />
         </body>
