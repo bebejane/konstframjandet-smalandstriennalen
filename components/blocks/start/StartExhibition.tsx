3d6688e51ea3def5822fcab8909c7fb030339a6c
Bebe Jane <bjornthief@gmail.com>
2023-05-16 10:35:10 +0200

    exhibitions start page

---
+++ components/blocks/start/StartExhibition.tsx
@@ -0,0 +1,41 @@
+import s from './StartExhibition.module.scss'
+import React from 'react'
+import { CardContainer, Card, Thumbnail } from '/components'
+import { useTranslations } from 'next-intl'
+import Link from '/components/nav/Link'
+
+export type Props = {
+  data: StartExhibitionRecord & {
+    exhibitions: ExhibitionRecord[]
+  }
+}
+
+export default function StartExhibition({ data: { exhibitions } }: Props) {
+  const t = useTranslations()
+
+  return (
+    <div className={s.container}>
+      <header>
+        <h2>{t('Menu.exhibitions')}</h2>
+        <Link href={'/utstallningar'} className="small">
+          {t('General.showAll')}
+        </Link>
+      </header>
+      <CardContainer>
+        {exhibitions.map(({ id, image, intro, title, slug, year }) =>
+          <Card key={id}>
+            <Thumbnail
+              image={image}
+              title={title}
+              intro={intro}
+              slug={`/${year.title}/utstallningar/${slug}`}
+              transformHref={false}
+              titleLength={50}
+              titleRows={1}
+            />
+          </Card>
+        )}
+      </CardContainer>
+    </div>
+  )
+}
