45a0bd278563a28970d7c2bf3f66a97a78d954ec
Bebe Jane <bjornthief@gmail.com>
2023-05-18 09:50:24 +0200

    t4est dates

--- pages/program/index.tsx
+++ pages/program/index.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/router";
 import { DatoSEO } from "dato-nextjs-utils/components";
 import { useTranslations } from "next-intl";
 import { pageSlugs } from "/lib/i18n";
+import { isAfter, isBefore, isSameDay } from 'date-fns';
 
 export type Props = {
   programs: ProgramRecord[]
@@ -23,8 +24,8 @@ export default function Program({ programs, programCategories }: Props) {
   const categoryFilter = ({ programCategory: { id } }: ProgramRecord) => !category || category === id
 
   const haveProgramItems = programs.filter(categoryFilter).length > 0
-  const pastPrograms = programs.filter(({ startDate, endDate }) => new Date(startDate) < new Date() && endDate);
-  const comingPrograms = programs.filter(({ startDate, endDate }) => new Date(startDate) >= new Date());
+  const pastPrograms = programs.filter(({ startDate, endDate }) => isAfter(new Date(), new Date(startDate)) && (!endDate || isAfter(new Date(), new Date(endDate))));
+  const comingPrograms = programs.filter(({ id }) => pastPrograms.find(({ id: pastId }) => pastId === id) === undefined);
 
   return (
     <>
