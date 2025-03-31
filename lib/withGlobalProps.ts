88a65429cfa0187e5cb15ce2748b92ddebc0319f
Bebe Jane <bjornthief@gmail.com>
2025-03-31 16:09:17 +0200

    fg

--- lib/withGlobalProps.ts
+++ lib/withGlobalProps.ts
@@ -19,9 +19,8 @@ export default function withGlobalProps(opt: any, callback: Function): GetStatic
 
   return async (context: GetStaticPropsContext) => {
 
-    if (context.params?.year && isNaN(parseInt(context.params?.year as string))) {
+    if (context.params?.year && isNaN(parseInt(context.params?.year as string)))
       return { notFound: true, revalidate };
-    }
 
     const years = await allYears(context.locale)
     let year = years.find(({ title }) => context.params?.year ? title === context.params?.year : title === years[0].title)
--- pages/om/index.tsx
+++ pages/om/index.tsx
@@ -1,30 +1,34 @@
-import s from './[about].module.scss'
-import withGlobalProps from "/lib/withGlobalProps";
+import s from './[about].module.scss';
+import withGlobalProps from '/lib/withGlobalProps';
 import { apiQuery } from 'dato-nextjs-utils/api';
-import { MainAboutDocument } from "/graphql";
+import { MainAboutDocument } from '/graphql';
 import { pageSlugs } from '/lib/i18n';
 
-export { default } from './[about]'
+export { default } from './[about]';
 
-export const getStaticProps = withGlobalProps({ queries: [] }, async ({ props, revalidate, context }: any) => {
+export const getStaticProps = withGlobalProps(
+	{ queries: [] },
+	async ({ props, revalidate, context }: any) => {
+		const yearId = props.year.id;
+		const { abouts } = await apiQuery(MainAboutDocument, {
+			variables: { locale: context.locale, yearId },
+			preview: context.preview,
+		});
+		const about = abouts[0] ?? null;
 
-  const yearId = props.year.id
-  const { abouts } = await apiQuery(MainAboutDocument, { variables: { locale: context.locale, yearId }, preview: context.preview })
-  const about = abouts[0] ?? null
+		if (!about) return { notFound: true };
 
-  if (!about)
-    return { notFound: true }
-
-  return {
-    props: {
-      ...props,
-      about,
-      page: {
-        section: 'about',
-        title: about.title,
-        slugs: pageSlugs('about', props.year.title, about._allSlugLocales)
-      } as PageProps
-    },
-    revalidate
-  };
-});
+		return {
+			props: {
+				...props,
+				about,
+				page: {
+					section: 'about',
+					title: about.title,
+					slugs: pageSlugs('about', props.year.title, about._allSlugLocales),
+				} as PageProps,
+			},
+			revalidate,
+		};
+	}
+);
