e5bda6123a63434853e7e4a97ee89bd2c3e92d53
Bebe Jane <bjornthief@gmail.com>
2026-02-11 16:58:07 +0100

    fix

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
-import s from './Button.module.scss'
-import cn from 'classnames'
-import { randomInt as rInt } from '/lib/utils'
+import { useEffect, useState } from 'react';
+import s from './Button.module.scss';
+import cn from 'classnames';
+import { randomInt as rInt } from '/lib/utils';
 
 export type Props = {
-  children: React.ReactNode
-  className?: string
-}
+	children: React.ReactNode;
+	className?: string;
+};
 
 export default function Button({ className, children }: Props) {
+	const [leftStyle, setLeftStyle] = useState({
+		left: '-10px',
+	});
+	const [rightStyle, setRightStyle] = useState({
+		right: '-10px',
+	});
 
-  const [leftStyle] = useState({ left: '-10px', transform: `skew(${rInt(0, 1) > 0 ? '-' : ''}${rInt(3, 10)}deg)` })
-  const [rightStyle] = useState({ right: '-10px', transform: `skew(${rInt(0, 1) > 0 ? '-' : ''}${rInt(3, 10)}deg)` })
+	useEffect(() => {
+		setLeftStyle((st) => ({
+			...st,
+			transform: `skew(${rInt(0, 1) > 0 ? '-' : ''}${rInt(3, 10)}deg)`,
+		}));
+		setRightStyle((st) => ({
+			...st,
+			transform: `skew(${rInt(0, 1) > 0 ? '-' : ''}${rInt(3, 10)}deg)`,
+		}));
+	}, []);
 
-  return (
-    <button className={cn(s.button, className)}>
-      <span>{children}</span>
-      <div className={s.left} style={leftStyle} />
-      <div className={s.right} style={rightStyle} />
-    </button>
-  )
+	return (
+		<button className={cn(s.button, className)}>
+			<span>{children}</span>
+			<div className={s.left} style={{ ...leftStyle }} />
+			<div className={s.right} style={{ ...rightStyle }} />
+		</button>
+	);
 }
